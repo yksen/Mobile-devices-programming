b90a711dc708e31db0429ae6730ad4ee2864666a
xen <selegakamil@gmail.com>
2022-10-26 00:02:43 +0200

    Create sample QuizApp

---
+++ Lista1/QuizApp/app/src/main/java/com/example/quizapp/Question.kt
@@ -0,0 +1,9 @@
+package com.example.quizapp
+
+class Question(
+    val question: String,
+    val answerOne: String,
+    val answerTwo: String,
+    val answerThree: String,
+    val correctAnswer: Int
+)
