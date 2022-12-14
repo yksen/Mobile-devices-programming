004b5fd9b56ba037c9df91ec6414c251178c9ce6
xen <selegakamil@gmail.com>
2022-12-14 21:04:18 +0100

    Create List and Detail fragments

---
+++ Lista3/StudentHardLife/app/src/main/java/com/example/studenthardlife/DetailFragment.kt
@@ -0,0 +1,19 @@
+package com.example.studenthardlife
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
