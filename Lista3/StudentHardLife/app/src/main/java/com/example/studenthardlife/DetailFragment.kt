99da96f14508b9cad8a9fb0ec6f138368c0b7fb8
xen <selegakamil@gmail.com>
2022-12-18 22:39:48 +0100

    Create add photo button, ask for permission to use camera, launch camera app

--- Lista3/StudentHardLife/app/src/main/java/com/example/studenthardlife/DetailFragment.kt
+++ Lista3/StudentHardLife/app/src/main/java/com/example/studenthardlife/DetailFragment.kt
@@ -1,17 +1,35 @@
 package com.example.studenthardlife
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.studenthardlife.databinding.FragmentDetailBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private lateinit var pictureAbsolutePath: Uri
     private var currentExerciseIndex: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +64,79 @@ class DetailFragment : Fragment() {
             exercises.removeAt(currentExerciseIndex)
             saveExercisesList(requireContext(), exercises)
         }
+        binding.addPhotoButton.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private val resultLauncherCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+                binding.imageView.setImageBitmap(imageBitmap)
+                pictureAbsolutePath = saveImage(imageBitmap)
+            }
+        }
+
+    private val requestCameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                launchCamera()
+            }
+        }
+
+    private fun showMessageOKCancel(message: String) {
+        AlertDialog.Builder(requireContext())
+            .setMessage(message)
+            .setPositiveButton("OK") { dialogInterface: DialogInterface, _: Int ->
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+                dialogInterface.dismiss()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+
+    private fun openCamera() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.CAMERA
+            ) ==
+                    PackageManager.PERMISSION_GRANTED -> {
+                launchCamera()
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.CAMERA
+            ) -> {
+                showMessageOKCancel(getString(R.string.rationale_camera))
+            }
+            else -> {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun launchCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        resultLauncherCamera.launch(intent)
+    }
+
+    private fun saveImage(bitmap: Bitmap): Uri {
+        var file = requireContext().getDir("myGalleryKotlin", Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return Uri.parse(file.absolutePath)
     }
 
 }
