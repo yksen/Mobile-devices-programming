d9f745573d8242779550f6ed1eadc694cce008d8
xen <selegakamil@gmail.com>
2023-01-05 22:22:01 +0100

    Add bottom navigation

--- Lista4/RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragment/CapitalsFragment.kt
+++ Lista4/RestCountriesApp/app/src/main/java/com/example/restcountriesapp/fragment/CapitalsFragment.kt
@@ -1,21 +1,21 @@
-package com.example.restcountriesapp.fragment.flags
+package com.example.restcountriesapp.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.restcountriesapp.databinding.FragmentFlagsBinding
+import com.example.restcountriesapp.databinding.FragmentCapitalsBinding
 
-class FlagsFragment : Fragment() {
-    private lateinit var binding: FragmentFlagsBinding
+class CapitalsFragment : Fragment() {
+    private lateinit var binding: FragmentCapitalsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFlagsBinding.inflate(inflater, container, false)
+        binding = FragmentCapitalsBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
